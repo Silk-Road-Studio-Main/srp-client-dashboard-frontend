0b61d6c0f771f1d47dbc9c2a2db9df731f5cd6a4
sujeevan-srp <sujeevan@silkroadpartners.lk>
2022-02-15 16:16:09 +0530

    Added hashrouter

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from "react-router-dom";
 import './assets/css/index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter base="/">
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
